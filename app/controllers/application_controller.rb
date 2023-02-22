c01a5a65787cb2da3b0ddef08703f7e7291ce206
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-02-22 02:41:43 +0000

    rock page playing works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    # Write ton of codes
+    #redirect_to("https://www.wikipedia.com")
+    #render({ :plain => rand(100) })
+    #render({:html =>"<h1> Hello, World!<h1>".html_safe})
+    render({:template => "game_templates/user_rock.html.erb" })
+  end
 end
