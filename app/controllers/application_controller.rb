b32212d82aa9d82d0c9479928cb1e91c30c15570
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-02-22 03:42:57 +0000

    lost of progress on  paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   layout(false)
-
+  def homepage
+render({:template =>"game_templates/rules.html.erb" })
+  end
   # Add your actions below this line
   # ================================
   def play_rock
@@ -10,4 +12,19 @@ class ApplicationController < ActionController::Base
     #render({:html =>"<h1> Hello, World!<h1>".html_safe})
     render({:template => "game_templates/user_rock.html.erb" })
   end
+  def play_paper
+    @comp_move= ["rock","paper","scissors"].sample
+    
+
+     if @comp_move == "rock" 
+      @outcome ="won"
+    elsif @comp_move == "paper" 
+       @outcome = "tied"
+     elsif @comp_move =="scissors" 
+        @outcome = "lost"
+      end 
+    
+
+    render({:template =>"game_templates/user_paper.html.erb" })
+      end
 end
